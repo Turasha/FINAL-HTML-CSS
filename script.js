defabfdb9c315ff7f3fc64630ca4a154d2ac186c
Turasha <teonagiox.t@gmail.com>
2024-03-05 17:30:00 +0400

    full done

---
+++ script.js
@@ -0,0 +1,12 @@
+let button = document.getElementById("submit");
+
+button.addEventListener("click", function() {
+    alert("sorry, this for will be able soon");
+});
+
+let navigationElement = document.getElementById('bar-ul');
+let burgerElement = document.getElementById('burger');
+
+burgerElement.addEventListener("click", function (){
+    navigationElement.classList.toggle("bar-ul-on");
+});
